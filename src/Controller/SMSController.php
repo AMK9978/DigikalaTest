6065445e81765cd29e452731071f142621626494
AmirMohammad Karimi <amk9978@gmail.com>
2020-10-23 19:30:00 +0330

    Calls happening successfully

--- src/Controller/SMSController.php
+++ src/Controller/SMSController.php
@@ -7,6 +7,8 @@ use App\Entity\SMSLog;
 use Carbon\Carbon;
 use GuzzleHttp\Exception\GuzzleException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use GuzzleHttp\Client;
 
@@ -14,39 +16,73 @@ use GuzzleHttp\Client;
 class SMSController extends AbstractController
 {
 
+    private $pubSubClient = null;
     /**
      * @Route("/create_sms", name="createSMS")
-     * @param string $number
-     * @param string $body
-     * @return SMS
+     * @param Request $request
+     * @return JsonResponse
      */
-    public function createSMS(string $number, string $body): SMS
+    public function createSMS(Request $request): JsonResponse
     {
+        $number = $request->request->get("number");
+        $body = $request->request->get("body");
         $sms = new SMS();
         $sms->setBody($body);
         $sms->setPhoneNumber($number);
         $this->getDoctrine()->getManager()->persist($sms);
         $this->getDoctrine()->getManager()->flush();
-        return $sms;
+
+        return new JsonResponse(["sms" => json_encode($sms)], 200);
     }
 
 
     /**
      * @Route("/sendSMS", name="sendSMS")
-     * @param SMS $sms
+     * @param Request $request
+     * @return JsonResponse
      */
-    public function sendSMS(SMS $sms)
+    public function sendSMS(Request $request)
     {
+        $sms = $this->getDoctrine()->getManager()
+            ->find(SMS::class, $request->get('sms_id'));
         try {
-            $number = random_int(1, 2);
-            if ($number == 1) {
-                $this->sendAPI1($sms);
-            } else {
-                $this->sendAPI2($sms);
-            }
+            return $this->messageBroker($sms);
         } catch (\Exception $e) {
+            return new JsonResponse(["msg" => "your request's queued"], 503);
         }
+    }
 
+    public function messageBroker(SMS $sms)
+    {
+        RedisController::getInstance()
+            ->rpush("", (array)$sms);
+        $pubSubClient = RedisController::getInstance()
+            ->pubSubLoop()->getClient();
+        $pubSubClient->rpush("", (array)$sms);
+        $pubSubClient->publish("ch1", $sms);
+        $number = random_int(1, 2);
+        if ($number == 1) {
+
+            try {
+                $this->sendAPI1($sms);
+                return new JsonResponse(["msg" => "your request's sent to api1"], 200);
+            } catch (\Exception $exception) {
+
+            }
+        } else {
+            $this->sendAPI2($sms);
+            return new JsonResponse(["msg" => "your request's sent to api2"], 200);
+        }
+    }
+
+    public function smsPop()
+    {
+        $client = RedisController::getInstance()->pubSubLoop()
+            ->getClient();
+        $client->subscribe("ch1");
+
+        var_dump(RedisController::getInstance()
+            ->rpop(""));
     }
 
     public function sendAPI1(SMS $sms)
@@ -109,7 +145,9 @@ class SMSController extends AbstractController
 
     public function pushTaskQueue(SMS $sms)
     {
-        $this->sendSMS($sms);
+        $request = new Request(['sms_id' => $sms->getId()]);
+        return;
+//        $this->sendSMS($request);
     }
 
 
